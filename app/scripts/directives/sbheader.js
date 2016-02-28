9a9643c4a8ceadaacde7c07cc1e663765394962c
Sergio Granada <chechogra@gmail.com>
2016-02-28 17:29:07 -0500

    Changes to template path 2

--- app/scripts/directives/sbheader.js
+++ app/scripts/directives/sbheader.js
@@ -9,7 +9,7 @@
 angular.module('smartBalanceApp')
   .directive('sbHeader', function () {
     return {
-      templateUrl: '/views/directives/header.html',
+      templateUrl: 'views/templates/header.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         //element.text('this is the sbHeader directive');
